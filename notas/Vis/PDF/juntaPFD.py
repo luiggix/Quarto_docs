1dbcf610980d0eafece2e38f601cc7171c9393ef
luiggix <luiggix@gmail.com>
2024-09-13 14:47:47 -0600

    inicio

---
+++ notas/Vis/PDF/juntaPFD.py
@@ -0,0 +1,14 @@
+from PyPDF2 import PdfWriter
+import glob
+
+notebook_pdf = [(f) for f in glob.glob('*.pdf')]
+
+print(notebook_pdf)
+
+merger = PdfWriter()
+
+for pdf in notebook_pdf:
+    merger.append(pdf)
+
+merger.write("Vis.pdf")
+merger.close()
